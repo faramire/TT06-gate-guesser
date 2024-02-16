30b5e01844056adead94255566cd0c69f2528b08
Fabio Ramirez Stern <github@faramire.de>
2024-02-16 13:51:26 +0100

    revert guesser.v

--- src/guesser.v
+++ src/guesser.v
@@ -6,10 +6,10 @@
 `define default_netname none
 
 module tt_um_faramire_gate_guesser (
-    input  wire [7:0] ui_in,    // Dedicated inputs:   switches 0 to 7
+    input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path:    switches 8 to F
-    output wire [7:0] uio_out,  // IOs: Output path:   gate outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
@@ -17,10 +17,7 @@ module tt_um_faramire_gate_guesser (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-
-    //TODO
-
-  assign uo_out  = 0;
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 

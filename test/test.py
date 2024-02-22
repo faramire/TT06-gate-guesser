6ed0873e246f551acc8de979597273b1d6088c83
Fabio Ramirez Stern <github@faramire.de>
2024-02-22 13:13:47 +0100

    Added logic tests

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2023 Uri Shaked <uri@tinytapeout.com>
+# SPDX-FileCopyrightText: © 2023 Uri Shaked <uri@tinytapeout.com>, 2024 Fabio Ramirez Stern <github@faramire.de>
 # SPDX-License-Identifier: MIT
 
 import cocotb
@@ -23,10 +23,19 @@ async def test_adder(dut):
   dut.rst_n.value = 1
 
   # Set the input values, wait one clock cycle, and check the output
-  dut._log.info("Test")
-  dut.ui_in.value = 20
-  dut.uio_in.value = 30
+  dut._log.info("Test 1")
+  dut.ui_in.value = int('01101111', 2)
+  dut.uio_in.value = int('11001011', 2)
 
   await ClockCycles(dut.clk, 1)
 
-  assert dut.uo_out.value == 50
+  assert dut.uo_out.value == 0
+
+  # Set the input values, wait one clock cycle, and check the output
+  dut._log.info("Test 2")
+  dut.ui_in.value = int('10110111', 2)
+  dut.uio_in.value = int('10110000', 2)
+
+  await ClockCycles(dut.clk, 1)
+
+  assert dut.uo_out.value == 255
